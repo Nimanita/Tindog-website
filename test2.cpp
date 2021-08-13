e60087d23aff8e939513173ee47b0eddfd9f2f23
Nimanita <mamatasingh2507@gmail.com>
2021-08-13 16:12:38 +0530

    Automationtest

---
+++ test2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+   string email="abc@gmail.com"
+   email = user2.email
+   spouse name = user.spousename
+   string phone ="1000"
+   email_address = "user.gmail"
+   exam_result = "student = 50%"
+    return 0;
+}
